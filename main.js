1eaa9e89938312501225b1116384f71fabfb18ec
Dile <duyle131103@gmail.com>
2024-10-08 09:22:59 +0700

    update UI

--- main.js
+++ main.js
@@ -111,7 +111,7 @@ const sr = ScrollReveal({
 
 sr.reveal(".home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .copy_right")
 sr.reveal(".home__image", { delay: 300, scale: 0.5 })
-sr.reveal(".service__card, .popular__card", { interval: 100 })
+sr.reveal(".service__card, .popular__card", { interval: 200 })
 sr.reveal(".about_leaf", { delay: 1000, origin: "right" })
 sr.reveal(".about__item__1-content, .about__item__2-img", { origin: "right" })
 sr.reveal(".about__item__2-content, .about__item__1-img", { origin: "left" })
